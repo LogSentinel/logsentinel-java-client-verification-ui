1fa53349d527d01affc263077caeaa6cd0bb2b4e
Stefan Genchev <stefan.genchev@sgsites.net>
2018-07-06 15:39:00 +0300

    Basic project structure

---
+++ src/main/java/com/logsentinel/verificationui/LogsentinelJavaClientVerificationUiApplication.java
@@ -0,0 +1,12 @@
+package com.logsentinel.verificationui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsentinelJavaClientVerificationUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsentinelJavaClientVerificationUiApplication.class, args);
+	}
+}
