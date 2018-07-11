1193bf0831b2ea621c4ab507073245766a1bdcfb
Stefan Genchev <stefan.genchev@sgsites.net>
2018-07-11 16:41:19 +0300

    Clean up code

--- src/main/java/com/logsentinel/verificationui/util/VisualizationUtil.java
+++ src/main/java/com/logsentinel/verificationui/util/VisualizationUtil.java
@@ -11,32 +11,31 @@ import java.util.Map;
 
 public class VisualizationUtil {
     private static Map<String, Integer> map = new HashMap<>();
-    private static Map<Integer, Integer> edges = new HashMap<>();
-
     private static List<Edge> edgesL = new ArrayList<>();
 
     public static TreeMap generateMap(int treeSize) {
         int id = 0;
+
         List<Integer> leaves = new ArrayList<>();
-        List<Node> nodes = new ArrayList<>();
 
         List<String> entriesI = new ArrayList<>();
 
         for (int i = 0; i <= treeSize - 1; i++) {
             leaves.add(id);
-            nodes.add(new Node(id, "l" + i));
             entriesI.add("l" + i);
             map.put("l" + i, id);
             id++;
         }
 
         List<String> concat = new ArrayList<>();
-        String leftOutside = "";
+
+        String leftAlone = "";
 
         double entries = Math.floor(treeSize / 2);
 
         while (entries != 0) {
             List<String> addedEntries = new ArrayList<>();
+
             for (int i = 0; i <= entriesI.size() - 1; i++) {
                 if (i % 2 == 0 && i != entriesI.size() - 1) {
                     concat.add(entriesI.get(i) + ":" + entriesI.get(i + 1));
@@ -47,30 +46,23 @@ public class VisualizationUtil {
                     edgesL.add(new Edge(id, map.get(entriesI.get(i + 1))));
                     id++;
 
-                    edges.put(id, map.get(entriesI.get(i)));
-                    edges.put(id, map.get(entriesI.get(i + 1)));
-
                     addedEntries.add(entriesI.get(i) + ":" + entriesI.get(i + 1));
                 }
                 else if (i % 2 == 0 && i == entriesI.size() - 1) {
-                    if (!leftOutside.equals("")) {
-                        concat.add(entriesI.get(i) + ":" + leftOutside);
-                        map.put(entriesI.get(i) + ":" + leftOutside, id);
+                    if (!leftAlone.equals("")) {
+                        concat.add(entriesI.get(i) + ":" + leftAlone);
+                        map.put(entriesI.get(i) + ":" + leftAlone, id);
 
                         edgesL.add(new Edge(id, map.get(entriesI.get(i))));
-                        edgesL.add(new Edge(id, map.get(leftOutside)));
-
+                        edgesL.add(new Edge(id, map.get(leftAlone)));
                         id++;
-                        addedEntries.add(entriesI.get(i) + ":" + leftOutside);
 
-                        edges.put(id, map.get(entriesI.get(i)));
-                        edges.put(id, map.get(leftOutside));
+                        addedEntries.add(entriesI.get(i) + ":" + leftAlone);
 
-
-                        leftOutside = "";
+                        leftAlone = "";
                     }
                     else {
-                        leftOutside = entriesI.get(i);
+                        leftAlone = entriesI.get(i);
                     }
                 }
             }
@@ -83,18 +75,17 @@ public class VisualizationUtil {
             entries = Math.floor(entries / 2);
         }
 
-        if (entriesI.size() == 1 && !leftOutside.equals("")) {
-            concat.add(entriesI.get(0) + ":" + leftOutside);
-            map.put(entriesI.get(0) + ":" + leftOutside, id);
+        if (entriesI.size() == 1 && !leftAlone.equals("")) {
+            concat.add(entriesI.get(0) + ":" + leftAlone);
+            map.put(entriesI.get(0) + ":" + leftAlone, id);
             edgesL.add(new Edge(id, map.get(entriesI.get(0))));
-            edgesL.add(new Edge(id, map.get(leftOutside)));
+            edgesL.add(new Edge(id, map.get(leftAlone)));
             id++;
         }
 
         if (entriesI.size() == 2) {
             concat.add(entriesI.get(0) + ":" + entriesI.get(1));
             map.put(entriesI.get(0) + ":" + entriesI.get(1), id);
-            edges.put(map.get(entriesI.get(0)), map.get(entriesI.get(1)));
             edgesL.add(new Edge(id, map.get(entriesI.get(0))));
             edgesL.add(new Edge(id, map.get(entriesI.get(1))));
             id++;
@@ -102,12 +93,12 @@ public class VisualizationUtil {
 
         List<Node> nodesL = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() <= nodes.size() - 1) {
+            if (entry.getValue() <= leaves.size() - 1) {
                 nodesL.add(new Node(entry.getValue(), "L" + (entry.getValue() + 1)));
             } else if (entry.getValue() + 1 == map.size()) {
                 nodesL.add(new Node(entry.getValue(), "MTH"));
             } else {
-                nodesL.add(new Node(entry.getValue(), ""));
+                nodesL.add(new Node(entry.getValue(), "N"));
             }
         }
 
