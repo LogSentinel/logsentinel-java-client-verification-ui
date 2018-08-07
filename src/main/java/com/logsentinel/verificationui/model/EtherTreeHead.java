0905a96f9e2d4b2e91bbb3c35b0dfb64d9760733
Bozho <bozhidar.bozhanov@gmail.com>
2018-08-07 11:52:55 +0300

    limiting duplicates in etherscan resultsa and passsing properly named parameter

--- src/main/java/com/logsentinel/verificationui/model/EtherTreeHead.java
+++ src/main/java/com/logsentinel/verificationui/model/EtherTreeHead.java
@@ -34,4 +34,29 @@ public class EtherTreeHead {
     public void setBlockNumber(String blockNumber) {
         this.blockNumber = blockNumber;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((hash == null) ? 0 : hash.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EtherTreeHead other = (EtherTreeHead) obj;
+        if (hash == null) {
+            if (other.hash != null)
+                return false;
+        } else if (!hash.equals(other.hash))
+            return false;
+        return true;
+    }
 }
--- src/main/java/com/logsentinel/verificationui/web/EtherscanController.java
+++ src/main/java/com/logsentinel/verificationui/web/EtherscanController.java
@@ -1,11 +1,11 @@
 package com.logsentinel.verificationui.web;
 
-import com.logsentinel.ApiException;
-import com.logsentinel.ApiResponse;
-import com.logsentinel.verificationui.model.EtherTransaction;
-import com.logsentinel.verificationui.model.EtherTransactionsResponse;
-import com.logsentinel.verificationui.model.EtherTreeHead;
-import com.logsentinel.verificationui.util.NetworkUtil;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +13,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
-import java.util.*;
-import java.util.stream.Collectors;
+import com.logsentinel.ApiException;
+import com.logsentinel.ApiResponse;
+import com.logsentinel.verificationui.model.EtherTransaction;
+import com.logsentinel.verificationui.model.EtherTransactionsResponse;
+import com.logsentinel.verificationui.model.EtherTreeHead;
+import com.logsentinel.verificationui.util.NetworkUtil;
 
 @RestController
 public class EtherscanController {
+    
+    private static final String MERKLE_ROOT_PREFIX_ENCODED = Hex.encodeHexString(":MERKLE_ROOT:".getBytes(StandardCharsets.UTF_8));
+    
     @RequestMapping(value = "/ether", method = RequestMethod.GET)
-    public List<EtherTreeHead> getMthsFromEthereum(@RequestParam("organizationId") String organizationId,
+    public List<EtherTreeHead> getMthsFromEthereum(@RequestParam("applicationId") String applicationId,
                                       @RequestParam("etherscanApiKey") String etherscanApiKey)
             throws ApiException, DecoderException, UnsupportedEncodingException {
 
-        String searchPattern = Hex.encodeHexString(organizationId.getBytes()) + "3a4d45524b4c455f524f4f543a";
+        String searchPattern = Hex.encodeHexString(applicationId.getBytes()) + MERKLE_ROOT_PREFIX_ENCODED;
 
         ApiResponse<EtherTransactionsResponse> response = NetworkUtil.getEtherscanTransactions(etherscanApiKey);
 
@@ -38,8 +44,11 @@ public class EtherscanController {
             for (EtherTransaction transaction : etherTransactions) {
                 String hash = transaction.getInput().substring(100, transaction.getInput().length() - 2);
 
-                foundTreeHeads.add(new EtherTreeHead(new String(Hex.decodeHex(hash.toCharArray()), "UTF-8"),
-                        transaction.getTimeStamp(), transaction.getBlockNumber()));
+                EtherTreeHead head = new EtherTreeHead(new String(Hex.decodeHex(hash.toCharArray()), "UTF-8"),
+                        transaction.getTimeStamp(), transaction.getBlockNumber());
+                if (!foundTreeHeads.contains(head)) {
+                    foundTreeHeads.add(head);
+                }
             }
 
             return foundTreeHeads;
